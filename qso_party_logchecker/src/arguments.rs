6e74da9530b1092aaac580737bdbc6a1c7a7432b
Ron Bondy ad0dx <ad0dx@yahoo.com>
2022-12-13 09:44:57 -0500

    Add Result<> return type from processing arguments to allow for tests to be written when processing arguments.

--- qso_party_logchecker/src/arguments.rs
+++ qso_party_logchecker/src/arguments.rs
@@ -1,21 +1,15 @@
-
-
-    use std::env;
-    use std::process;
     
     use crate::file_utils::file_exists;
 
     // Process the command line arguments and return the name of the config file
     // If an error occurs (config file not provided) then the process exits
     // If the config file does not exist, issue an error and exit the process 
-    pub fn process_arguments() -> String {
-
-        let args: Vec<String> = env::args().collect();
-    
+    pub fn process_arguments(args: &Vec<String>) -> Result<String, i32> {
+   
         let nargs = args.len();
         println!("Number of arguments: {}", nargs);
         let mut i = 1;
-        for s in &args {
+        for s in args {
             println!("{}) {}", i, s);
             i += 1;
         }
@@ -27,7 +21,8 @@
         if nargs != 2 {
             println!("Usage: qso_party_logchecker config.txt");
             println!("       cargo run config.txt");
-            process::exit(1);
+
+            return Err(1);
         }
         
         let config = &args[1];
@@ -37,8 +32,9 @@
         // if the configuration file does not exist, issue an error and exit the process
         if false == exists {
             println!("Error: File {} does not exist :-(", config);
-            process::exit(2);
+
+            return Err(2);
         }
 
-        config.to_string()
+        Ok(config.to_string())
     }
